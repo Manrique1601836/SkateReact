4e3253412ad948dcd713a6b202cab80cd5b81d17
Manrique1601836 <manriqueedwin558@gmail.com>
2025-12-04 19:10:13 -0500

    commit 2/..

--- src/pages/hoot.jsx
+++ src/pages/hoot.jsx
@@ -1,6 +1,49 @@
 
+import SkateHoot from "./img/skatehoot.png"
+import "./style/hoot.css"
+import PoPShovit from "./media/PoPShovit.mp4"
+import KicKFlip from "./media/kickFlip.mp4"
+import Olli from "./media/Olli.mp4"
+
 export default function Hoot(){
     return(
-        <p>contenido principal</p>
+        <div className="hoot">
+            <img src={SkateHoot} alt="SkateHoot" className="img-skate-hoot"/>
+            <div className="trucos-skates">
+                <div className="truco">
+                    <video 
+                        src={PoPShovit} 
+                        className="video-hoot" 
+                        controls  // muestra play, pausa, etc.
+                        autoPlay  // empieza solo
+                        loop      // se repite
+                        playbackRate="2.0"
+                    />
+                    <h1>POP SHOVIT</h1>
+                </div>
+                <div className="truco">
+                    <video 
+                        src={KicKFlip} 
+                        className="video-hoot" 
+                        controls  // muestra play, pausa, etc.
+                        autoPlay  // empieza solo
+                        loop      // se repite
+                        playbackRate="2.0"
+                    />
+                    <h1>KICK FLIP</h1>
+                </div>
+                <div className="truco">
+                    <video 
+                        src={Olli} 
+                        className="video-hoot" 
+                        controls  // muestra play, pausa, etc.
+                        autoPlay  // empieza solo
+                        loop      // se repite
+                        playbackRate="2.0"
+                    />
+                    <h1>Olli</h1>
+                </div>
+            </div>
+        </div>
     )
 }
